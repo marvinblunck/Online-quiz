0b744c432715a2e05aaca141e1f3c9ba1383f8be
marvin blunck <marvinblunck@gmail.com>
2023-04-15 15:58:12 -0700

    added classes and connected to js

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,14 @@
+var buttons=document.querySelectorAll("button");
+
+var question1=document.getElementById("question-1");
+
+var question2=document.getElementById("question-2");
+
+var question3=document.getElementById("question-3");
+
+var question4=document.getElementById("question-4");
+
 console.log("hello world");
+
+
+var gameOver=document.querySelector("#game-over");
