9f8812cd2a3e1ae4db5e61d15ceb24ebbae11b72
marvin blunck <marvinblunck@gmail.com>
2023-04-15 17:19:05 -0700

    got buttons to hid and show while connecting them

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,52 @@
-var buttons=document.querySelectorAll("button");
+var buttons = document.querySelectorAll(".answer");
 
-var question1=document.getElementById("question-1");
+var question1 = document.getElementById("question-1");
 
-var question2=document.getElementById("question-2");
+var question2 = document.getElementById("question-2");
 
-var question3=document.getElementById("question-3");
+var question3 = document.getElementById("question-3");
 
-var question4=document.getElementById("question-4");
+var question4 = document.getElementById("question-4");
 
 console.log("hello world");
 
 
-var gameOver=document.querySelector("#game-over");
+var gameOver = document.querySelector("#game-over");
+
+buttons.forEach(button => {
+    button.addEventListener("click", function (event) {
+        var answer = event.target.dataset.select;
+        if (answer === "true") {
+            console.log("correct");
+        } else {
+            console.log("incorrect");
+        }
+
+        var q1Array= Array.from(question1.classList)
+        var q2Array= Array.from(question2.classList)
+        var q3Array= Array.from(question3.classList)
+        var q4Array= Array.from(question4.classList)
+        
+        if (!q1Array.includes("hide")) {
+            question1.classList.add("hide");
+
+            question2.classList.remove("hide");
+        }
+        
+        else if (!q2Array.includes("hide")) {
+            question2.classList.add("hide");
+            question3.classList.remove("hide");
+        }
+        else if (!q3Array.includes("hide")) {
+            question3.classList.add("hide")
+            question4.classList.remove("hide")
+        }
+        else if (!q4Array.includes("hide")) {
+            question4.classList.add("hide")
+            gameOver.classList.remove("hide")
+        }
+
+
+        console.log(event.target.dataset.select)
+    })
+});
